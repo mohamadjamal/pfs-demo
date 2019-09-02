d8caf8f234ec1bbce647907588cec185b8b67365
Mohamad Jamal AJ <mohamadjamal.aj@cognizant.com>
2019-09-02 21:21:27 +0530

    updated square funciton

--- src/main/java/com/cognizant/tao/cloud/Greeting.java
+++ src/main/java/com/cognizant/tao/cloud/Greeting.java
@@ -4,6 +4,7 @@ import java.util.function.Function;
 
 public class Greeting implements Function<String, String> {
 
+	@Override
 	public String apply(String name) {
 		return "Welcome to Cloud Foucs Group : " + name;
 	}
